e5152eeaccabba05bac9c29acd8ed7ba0f0c7f4d
up201907216 <up201907216@edu.fe.up.pt>
2022-01-28 18:48:07 +0000

    File rename

--- net_config/tux3.sh
+++ net_config/tux3.sh
@@ -10,8 +10,5 @@ ip -family inet neigh flush any
 
 # Guide 4
 ip route add 172.16.11.0/24 via 172.16.10.254
-ip route show
-
 ip -family inet neigh flush any
-
 ip route add default via 172.16.10.254
